4e31b092a57dd5b29cc0238f9efe692318bbc94f
monkeymr014 <monkeymr014@gmail.com>
2022-11-10 15:22:06 +0100

    2 dział

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const App = () => (
+    <div className="wrpapper">
+        <h1 className="mainHeader">Hello</h1>
+    </div>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from './App';
 
-ReactDOM.render( <
-    h1 > sss < /h1 > ,
+
+ReactDOM.render(
+    <App />,
     document.getElementById('root')
 );
