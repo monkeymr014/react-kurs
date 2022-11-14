4bf46ee4437d3c76fd7231a2ad98d9b38ed4583b
monkeymr014 <monkeymr014@gmail.com>
2022-11-14 14:10:57 +0100

    lesson 3.3

--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -1,9 +1,16 @@
 import React from "react";
 import './ListItem.css'
+import img from '../../../assets/image/logo.png'
+
 const ListItem = () =>
 (
-   <li className="listItemWrapper">
-      Item
+   <li className="listItem__wrapper">
+      <img src={img} className="listItem__image" />
+      <div>
+         <h2 className="listItem__name" >name</h2>
+         <p className="listItem__descrition"> asdl;ka snd laskdn lasknd lksn dlaksdn</p>
+         <button className="listItem__button">fb</button>
+      </div>
    </li>
 );
 
