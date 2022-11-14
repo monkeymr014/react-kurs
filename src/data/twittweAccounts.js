9809178341e3a0ccdcbb1c74a9591dfcc2d03cf5
monkeymr014 <monkeymr014@gmail.com>
2022-11-14 13:04:15 +0100

    lesson 3.1

---
+++ src/data/twittweAccounts.js
@@ -0,0 +1,33 @@
+import img1 from '../assets/images/logo.png';
+
+export const twitterAcconutns = [
+
+   {
+      image: img1,
+      name: 'name',
+      description: 'description',
+      twitterLink: 'link',
+
+   },
+   {
+      image: img1,
+      name: 'name',
+      description: 'description',
+      twitterLink: 'link',
+
+   },
+   {
+      image: img1,
+      name: 'name',
+      description: 'description',
+      twitterLink: 'link',
+
+   },
+   {
+      image: img1,
+      name: 'name',
+      description: 'description',
+      twitterLink: 'link',
+
+   },
+]
