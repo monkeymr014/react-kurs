c88622ccdb75c13f1fb1e4b63747eef77bd32604
monkeymr014 <monkeymr014@gmail.com>
2022-11-15 11:47:42 +0100

    lesson 4.1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ListWrpapper from "./components/ListWrapper/ListWrapper";
+import MyComponent from "./components/MyComponent/MyComponent";
 import './index.css'
 
 const App = () => (
     <div>
         <ListWrpapper/>
+        <MyComponent/>
     </div>
 );
 
