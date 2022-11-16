818733da05c18cb4ae588c97936a1cb7f9dabcdc
monkeymr014 <monkeymr014@gmail.com>
2022-11-16 11:34:35 +0100

    lessson 4.10

--- src/components/Form/Forms.js
+++ src/components/Form/Forms.js
@@ -4,39 +4,41 @@ import styles from './Form.module.scss';
 const Form = ({submitFn}) => (
     <div className={styles.wrapper}>
         <h2>Add new twitter account</h2>
-        <form 
-            className={styles.form__form} 
+        <form autoComplete='off'
+            className={styles.form} 
             onSubmit={submitFn}
         >
-            <div className={styles.form__item}>
+            <div className={styles.item}>
                 <input 
                     type="text" 
                     name="name" 
                     id="name" placeholder=" " maxLength="30"
+                    required
                 />
                 <label htmlFor="name">Name</label>
-                <div className={styles.forms__item__bar}></div>
+                <div className={styles.bar}></div>
             </div>
-            <div className={styles.form__item}>
+            <div className={styles.item}>
                 <input
                     type="text"
                     name="link"
                     id="link" placeholder=" "
+                    required
                 />
                 <label htmlFor="link">Link</label>
-                <div className={styles.forms__item__bar}></div>
+                <div className={styles.bar}></div>
             </div>
-            <div className={styles.form__item}>
+            <div className={styles.item}>
                 <input
                     type="text"
                     name="image"
                     id="image" 
-                    placeholder=" " 
+                    placeholder=" "
                 />
                 <label htmlFor="image">Image</label>
-                <div className={styles.forms__item__bar}></div>
+                <div className={styles.bar}></div>
             </div>
-            <div className={styles.form__item}>
+            <div className={styles.item}>
                 <textarea
                     name="description"
                     id="description"
@@ -46,7 +48,7 @@ const Form = ({submitFn}) => (
                 <div className={styles.forms__item__bar}></div>
             </div>
             <button 
-                className={styles.form__button}
+                className={styles.button}
                 type="submit"
             >
                 add new item
--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -1,32 +1,37 @@
 import React from "react";
-import PropTypes  from "prop-types";
-import styles from  './ListItem.module.scss'
+import PropTypes from "prop-types";
+import styles from './ListItem.module.scss'
 
 const ListItem = ({
    name,
    image,
    description,
    twitterLink,
-}) =>
-(
-   <li className={styles.wrapper}>
-      <img src={image} className={styles.image} alt="name" />
-      <div>
-         <h2 className={styles.name} >{name}</h2>
-         <p className={styles.descrition}> {description}</p>
-         <a href="{twitterLink}" target="_blank" className={styles.button}>twitterLink</a>
-      </div>
-   </li>
-);
+} ) => {
+   const ImageTag = image ? 'img' : 'div';
+
+   return (
+
+      <li className={styles.wrapper}>
+         <ImageTag src={image} className={image ? styles.image : styles.imageNone } alt="name" />
+         <div>
+            <h2 className={styles.name} >{name}</h2>
+            <p className={styles.descrition}> {description}</p>
+            <a href="{twitterLink}" target="_blank" className={styles.button}>twitterLink</a>
+         </div>
+      </li>
+   )
+};
 
 ListItem.propTypes = {
    name: PropTypes.string.isRequired,
-   image: PropTypes.string.isRequired,
+   image: PropTypes.string,
    description: PropTypes.string,
    twitterLink: PropTypes.string,
 };
 
 ListItem.defaultProps = {
+   image: null,
    description: 'brak danych'
 }
 export default ListItem;
