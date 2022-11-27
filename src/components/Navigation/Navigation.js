b1b91a52268bf6c0e44216f74a28b26bada08caa
monkeymr014 <monkemyr014@gmail.com>
2022-11-27 15:26:48 +0100

    lesson 5.6 compolete

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,18 +2,32 @@ import React from "react";
 import { NavLink } from "react-router-dom"
 import styles from "./Navigation.module.scss"
 
+
+const classNameFunc = ({ isActive }) => (!isActive ? styles.navItemLink : styles.navItemLinkActive);
+
+
 const Navigation = () => (
    <nav>
       <ul className={styles.wrapper}>
          <li className={styles.navItem}>
-            <NavLink 
-            className={({ isActive }) => {styles.navItemLink + (isActive ? "Active" : "")} to="/">Twiter</NavLink>
+            <NavLink
+               className={classNameFunc}
+               to="/s">
+               Twiter
+            </NavLink>
          </li>
          <li className={styles.navItem}>
-            <NavLink  className={styles.navItemLink} to="/articles">Articles</NavLink>
+            <NavLink
+               className={classNameFunc}
+               to="/articles">
+               Articles
+            </NavLink>
          </li>
          <li className={styles.navItem}>
-         <NavLink className={styles.navItem} className={styles.navItemLink} to="/notes">Notes</NavLink>
+            <NavLink
+               className={classNameFunc}
+
+               to="/notes">Notes</NavLink>
          </li>
       </ul>
    </nav>
